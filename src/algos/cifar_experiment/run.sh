cf1a6c74c6ce23cc9d393a71717c06005e510dbb
Siddharth Nayak <siddharthnayak98@gmail.com>
2019-08-07 21:25:27 +0530

    kubernete files

---
+++ src/algos/cifar_experiment/run.sh
@@ -0,0 +1,20 @@
+# Note: all paths referenced here are relative to the Docker container.
+#
+# Add the Nvidia drivers to the path
+export PATH="/usr/local/nvidia/bin:$PATH"
+export HOME="/storage/home/sidnayak"
+export LD_LIBRARY_PATH="/usr/local/nvidia/lib:$LD_LIBRARY_PATH"
+# Tools config for CUDA, Anaconda installed in the common /tools directory
+source /tools/config.sh
+# Activate your environment
+source activate py35
+# Change to the directory in which your code is present
+cd /storage/home/sidnayak/Transfer-Matching-Networks/src/algos/cifar_experiment/
+
+
+# Run the code. The -u option is used here to use unbuffered writes
+# so that output is piped to the file as and when it is produced.
+
+# python -u Agent_detect.py --load_model=1 --epoch=10 &> out
+# python -u ssd_random.py --epoch=20 &> outputs/out_random_newReward
+python -u cifarTest.py --tensorboard=1 --rm_runs=1
